90fabe16c46ac57bdf58c32c793e6838f23da0f4
Unknown <mjhaveri@bu.edu>
2017-11-14 00:59:07 -0500

    started multi files

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -8,3 +8,27 @@
 
 // Put your code here.
 	@i 
+	M=0 
+	@MULT
+	M=0 
+	@0 
+	D=M 
+	@R_ZERO
+	M=D 
+	@1 
+	D=M 
+	@R_ONE
+	M=D
+(LOOP)
+	@R_ZERO
+	D=M
+	@i 
+	D=M
+	@R_ONE
+	D=D-M
+	@LOOP
+	D;JGT
+		
+(END)
+	@END
+	0;JMP
