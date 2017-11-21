50abc82664ae2c26a6280bb733c7f1851ded80e1
moniljhaveri <mjhaveri@bu.edu>
2017-11-20 21:15:57 -0500

    finished mult and working on fill

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -8,3 +8,54 @@
 
 // Put your code here.
 	@i 
+	M=1 
+	@MULT 
+	M=0 
+	@0 
+	D=M
+	@RZERO 
+	M=D
+	@1 
+	D=M 
+	@RONE 
+	M=D 
+	@2 
+	M=0
+	@RZERO 
+	D=M
+	@END
+	D;JEQ
+	@RONE 
+	D=M
+	@END
+	D;JEQ
+(LOOP)
+	@i 
+	D=M 
+	@RONE
+	D=D-M 
+	@SAVE
+	D;JGT 
+	@RZERO
+	D=M 
+	@MULT
+	M=D+M 
+	@i 
+	D=M 
+	@i 
+	D=M
+	M=M+1
+	@LOOP
+	0;JMP
+(SAVE) 
+	@MULT
+	D=M
+	@2
+	M=D 
+	@END 
+	0;JMP 
+(END) 
+	@END 
+	0;JMP
+	
+	 
