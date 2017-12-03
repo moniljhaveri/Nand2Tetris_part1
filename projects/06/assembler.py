da44d6cad5c631bddf2af684625777f41e0ac2d7
Unknown <mjhaveri@bu.edu>
2017-12-02 22:02:45 -0500

    refactored the code to make the assembler object oriented

--- projects/06/assembler.py
+++ projects/06/assembler.py
@@ -1,20 +1,43 @@
 import glob, os 
 import sys
 
-def parse_file(line, first_pass): 
+class Assembler: 
+    def __init__(self, file_path): 
+        self.file_path = file_path
+        self.commands = [] 
+        self.command = self.open_file()
+        self.symbol_table = {}
+    
+    def open_file(self): 
+        with open(self.file_path, "r") as file: 
+            for line in file: 
+                self.commands = self.parse_file(line, self.commands)
+        print(self.commands)
 
-    if (line == "\n"): 
-        pass
-    elif "//" in line: 
-       comm_split = line.split("//") 
-       if len(comm_split[0]) != 0 : 
-           first_pass.append(comm_split[0])
+    def parse_file(self, line, first_pass): 
+        if (line == "\n"): 
+            pass
+        elif "//" in line: 
+            comm_split = line.split("//") 
+            if len(comm_split[0]) != 0 : 
+                first_pass.append(comm_split[0])
+        else: 
+            first_pass.append(line.splitlines()[0]) 
+        return first_pass
+
+
+def main(): 
+    num_args = len(sys.argv)
+    if num_args < 2: 
+        print("Need to have an input file to run a program")
+        return 
     else: 
-        first_pass.append(line) 
-    return first_pass
+        file_path = str(sys.argv[1])
+        a = Assembler(file_path)
+    #with open(file_path, "r") as file:
+    #    for line in file: 
+        #clean_input = parse_file(line, clean_input)
+    return 
 
-file_path = "/home/mjhaveri/develop/Nand2Tetris_part1/projects/06/add/Add.asm"
-clean_input = []
-with open(file_path, "r") as file:
-    for line in file: 
-        first_pass = parse_file(line, clean_input)
+if __name__ == "__main__": 
+    main() 
