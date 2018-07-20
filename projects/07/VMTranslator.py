d3226577d2cadd85f3db487f00984f674df80fcb
Unknown <mjhaveri@bu.edu>
2018-07-20 00:33:49 -0400

    created first unit test

--- projects/07/VMTranslator.py
+++ projects/07/VMTranslator.py
@@ -2,19 +2,34 @@ class VMParse:
     def __init__(self, file_path):
         self.file_path = file_path
         self.inst_st = []
+        self.n = 0
+        self.parse()
+        self.ind = 0
 
     def parse(self):
         with open(self.file_path) as file:
             #            read_data = file.read()
+            self.ind = 0
+            self.inst_st = []
             for line in file:
                 line_split = line.split("//")
                 line_seg = line_split[0].replace("\n", "")
                 if 0 < len(line_seg):
                     self.inst_st.append(line_seg)
+        self.n = len(self.inst_st)
+
+    def hasMoreCommands(self):
+        if self.ind < self.n:
+            return True
+        return False
+
+    def num_instr(self):
+        return self.n
 
 
 print("hello world")
 vm_obj = VMParse("./StackArithmetic/SimpleAdd/SimpleAdd.vm")
 
-vm_obj.parse()
-print(vm_obj.inst_st)
+
+def test_answer():
+    assert vm_obj.num_instr() == 3
